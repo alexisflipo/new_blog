3aa3426ec52999e7248dc2e1b614f3c5a26f9db2
Alexis Flipo <alexis.flipo1@gmail.com>
2020-08-10 10:17:32 +0200

    Added routes

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Creating Articles" do
+  scenario "A user creates a new article" do
+
+    visit "/"
+
+    click_link "New Article"
+
+    fill_in "Title", with: "Creating a blog"
+    fill_in "body", with: "Lorem ipsum dolor sit amet."
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eq(articles_path)
+  end
+end
