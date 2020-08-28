c4976e6344a7c6c501f1715c34bf1ad3ba15028c
Alexis Flipo <alexis.flipo1@gmail.com>
2020-08-28 15:06:16 +0200

    Edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
   def index
     @articles = Article.all
   end
@@ -16,8 +17,21 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
+
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article has been updated"
+      redirect_to @article
+    else
+      flash.now[:alert] = "Article has not been updated"
+      render :edit
+    end
   end
 
   protected
@@ -29,6 +43,10 @@ class ArticlesController < ApplicationController
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
   def article_params
     params.require(:article).permit(:title, :body, :author)
