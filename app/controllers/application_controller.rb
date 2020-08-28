fd3a1522f4ae14de008b4511c7262cde352d6093
Alexis Flipo <alexis.flipo1@gmail.com>
2020-08-28 14:34:10 +0200

    Spec status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
+  protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :resource_not_found
+
+  protected
+
+  def resource_not_found
+
+  end
 end
