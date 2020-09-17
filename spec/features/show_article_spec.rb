8af387d1a12699d7d3cbe6267857f9a5cd620b03
Alexis Flipo <alexis.flipo1@gmail.com>
2020-09-17 15:54:06 +0200

    Update spec to verify owner sees edit and delete buttons

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.feature "Show article" do
 
   before do
-     john = User.create!(email: "john@example.com", password: "password")
+     @john = User.create!(email: "john@example.com", password: "password")
      @fred = User.create!(email: "fred@example.com", password: "password")
-    @article = Article.create(title: "first title", body: "body", author: "alex", user: john)
+    @article = Article.create(title: "first title", body: "body", author: "alex", user: @john)
   end
 
   scenario 'to non-signed in user hide the Edit and Delete buttons' do
@@ -33,5 +33,19 @@ end
 
   expect(page).not_to have_link("Edit Article")
   expect(page).not_to have_link("Delete Article")
+end
+
+scenario 'A signed in owner sees bothe the Edit and Delete buttons' do
+  login_as(@john)
+  visit "/"
+
+  click_link @article.title
+
+  expect(page).to have_content(@article.title)
+  expect(page).to have_content(@article.body)
+  expect(current_path).to eq(article_path(@article))
+
+  expect(page).to have_link("Edit Article")
+  expect(page).to have_link("Delete Article")
 end
 end
