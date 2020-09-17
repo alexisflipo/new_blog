34ee394613390e773517a117e3b4c0a7ee08bf0f
Alexis Flipo <alexis.flipo1@gmail.com>
2020-09-17 15:52:03 +0200

    restrictions

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -4,11 +4,11 @@ RSpec.feature "Show article" do
 
   before do
      john = User.create!(email: "john@example.com", password: "password")
-    login_as(john)
+     @fred = User.create!(email: "fred@example.com", password: "password")
     @article = Article.create(title: "first title", body: "body", author: "alex", user: john)
   end
 
-  scenario 'A user goes to show page' do
+  scenario 'to non-signed in user hide the Edit and Delete buttons' do
   visit "/"
 
   click_link @article.title
@@ -16,5 +16,22 @@ RSpec.feature "Show article" do
   expect(page).to have_content(@article.title)
   expect(page).to have_content(@article.body)
   expect(current_path).to eq(article_path(@article))
+
+  expect(page).not_to have_link("Edit Article")
+  expect(page).not_to have_link("Delete Article")
+end
+
+  scenario 'to non-owner hide the Edit and Delete buttons' do
+  login_as(@fred)
+  visit "/"
+
+  click_link @article.title
+
+  expect(page).to have_content(@article.title)
+  expect(page).to have_content(@article.body)
+  expect(current_path).to eq(article_path(@article))
+
+  expect(page).not_to have_link("Edit Article")
+  expect(page).not_to have_link("Delete Article")
 end
 end
