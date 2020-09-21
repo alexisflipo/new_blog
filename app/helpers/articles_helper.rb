20f502f55e03ca30a69e8777eef1c35d47a3d4ff
Alexis Flipo <alexis.flipo1@gmail.com>
2020-09-21 17:16:38 +0200

    Adding comments

---
+++ app/helpers/articles_helper.rb
@@ -0,0 +1,6 @@
+module ArticlesHelper
+  def persisted_comments(comments)
+    comments.reject { |comment| comment.new_record? }
+
+  end
+end
---
+++ spec/features/adding_comments_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature "Adding reviews to Articles" do
+  before do
+    @john = User.create(email: "john@exemple.com", password: "password")
+    @fred = User.create(email: "fred@exemple.com", password: "password")
+    @article = Article.create!(title: "Title One", body: "Body of article one", author: "john", user: @john)
+  end
+
+  scenario "permits a signed in user to write a review" do
+    login_as(@fred)
+
+    visit "/"
+
+    click_link @article.title
+    fill_in "New Comment", with: "An amazing article"
+    click_button "Add Comment"
+
+    expect(page).to have_content("Comment has been successfully created")
+    expect(page).to have_content("An amazing article")
+    expect(current_path).to eq(article_path(@article))
+  end
+
+end
